6b3297d0fde13a37d37df959227a4f12f4bc557e
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2021-11-17 12:11:29 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import ReactBootstrap from 'react-bootstrap';
+import Products from "./Components/products";
+
+function App() {
+  return (
+    <Products></Products>
+  );
+}
+
+export default App;
